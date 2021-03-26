441ef2298689d4301a420f480af9c2c8d250881f
Vikassinghmehra <vikassinghmehra@gmail.com>
2021-03-26 10:52:43 +0530

    added second method to solve reverse array

--- Reversearray.java
+++ Reversearray.java
@@ -1,3 +1,4 @@
+/*
 class Reversearray{
 
     public static void main(String args[]){
@@ -12,6 +13,10 @@ class Reversearray{
     public static  void reverseArray(int a[]){
         
         //System.out.println(a.length);
+        if(a.length<=1){
+            System.out.print("Only one or no element");
+        }else
+        {
         for(int i=0,end=a.length-1;i<a.length/2;i++,end--){
                 int temp=0;
                 temp=a[i];
@@ -23,4 +28,37 @@ class Reversearray{
             System.out.print(a[i] + " " );
         }
     }
+    }
+}*/
+
+
+class Reversearray{
+
+    public static void main(String args[]){
+        int a[]={1,2,3,4,5,6,7};
+        int size=a.length;
+        for(int i=0;i<size;i++){
+            System.out.print(a[i] + " " );
+        }
+        System.out.println();
+        reverseArray(a,0,size-1);
+        for(int i=0;i<size;i++){
+            System.out.print(a[i] + " " );
+        }
+
+    }
+    public static  void reverseArray(int a[],int start,int end){
+        
+        //System.out.println(a.length);
+        
+       
+                if(start<=end){
+                int temp=0;
+                temp=a[start];
+                a[start]=a[end];
+                a[end]=temp;
+                reverseArray(a,start+1,end-1);
+                }
+    }
+    
 }
