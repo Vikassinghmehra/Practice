b59e69230819100c65c8c3d69b81692883231484
Vikassinghmehra <vikassinghmehra@gmail.com>
2021-08-10 08:45:33 +0530

    added more methods to solve the questions

--- KthSmallestElementInArray.java
+++ KthSmallestElementInArray.java
@@ -2,6 +2,7 @@
 //using quick select or partitioned technique
 
 
+/*
 public class KthSmallestElementInArray {
 
 
@@ -61,3 +62,50 @@ public class KthSmallestElementInArray {
     }
 
 }
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class KthSmallestElementInArray {
+    public static void main(String[] args) {
+        int a[]={2,34,6,7,8,1,3};
+
+        kthSmallest(a,3);
+
+    }
+
+    private static void kthSmallest(int[] a,int k) {
+        TreeMap<Integer,Integer>map=new TreeMap<>();
+
+        for(int i=0;i<a.length;i++){
+            if(map.containsKey(a[i])){
+                map.put(a[i], map.get(a[i]+1));
+            }else{
+                map.put(a[i],1);
+            }
+
+        }
+        int t=0;
+
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+
+            if(t==k-1) {
+                try {
+                    int key = entry.getKey();
+                    int value = entry.getValue();
+                    System.out.println(key + " => " + value);
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+
+            t++;
+        }
+
+    }
+}
+
+
--- MoveNegToLeftAndPosToRight.java
+++ MoveNegToLeftAndPosToRight.java
@@ -3,13 +3,14 @@
 public class MoveNegToLeftAndPosToRight {
 
     public static void main(String args[]){
-        int a[]={1,2,3,4,5,6,7};
+        int a[]={1,2,3,-4,-5,-6,-7};
         int size=a.length;
         for(int i=0;i<size;i++){
             System.out.print(a[i] + " " );
         }
-        move(a,size-1);
-
+        //move(a,size-1);
+        twoPointerApproach(a,0,size-1);
+        System.out.print("\n");
         for(int i=0;i<size;i++){
             System.out.print(a[i] + " " );
         }
@@ -21,13 +22,37 @@ public class MoveNegToLeftAndPosToRight {
             for(int i=0;i<size;i++){
 
                 if(a[i]<0){
+
                     if(i!=j){
                         temp=a[i];
                         a[i]=a[j];
                         a[j]=temp;
                     }
+
                     j++;
                 }
             }
+    }
+
+    public static void twoPointerApproach(int a[] ,int left ,int right){
+
+
+        while (left<=right){
+
+            if(a[left]<0 && a[right]>0){
+                left++;right--;
+            }else if(a[left]>0 && a[right]<0){
+                a[left]^=a[right];
+                a[right]^=a[left];
+                a[left]^=a[right];
+                left++;right--;
+            }else if(a[left]>0 && a[right]>0){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+
     }
 }
