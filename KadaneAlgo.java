31ab41532f7482266b805778b12e4b6413850bd5
Vikassinghmehra <vikassinghmehra@gmail.com>
2021-08-11 10:22:41 +0530

    added some modification in kadan's func

--- KadaneAlgo.java
+++ KadaneAlgo.java
@@ -16,22 +16,26 @@ public class KadaneAlgo {
     }
 
     private static int kadanesAlgo(int[] a, int length) {
+        int max_sum=0;
+
         if(a.length==0){
-            return 0;
+            max_sum=0;
         }else if(a.length==1){
-            return a[0];
+            max_sum=a[0];
         }else {
 
-            int sum_till_now=a[0],max_sum=a[0];
-
+            int sum_till_now=a[0];
+            max_sum=a[0];
             for (int i=1;i<length;i++){
 
                 sum_till_now=Math.max(a[i],a[i]+sum_till_now);
                 max_sum=Math.max(sum_till_now,max_sum);
 
             }
-            return max_sum;
+
         }
+
+        return max_sum;
 
     }
 
